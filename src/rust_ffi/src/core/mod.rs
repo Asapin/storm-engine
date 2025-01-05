46fd4995106b2cfaec56aed7d11ce0d1d09f8413
Asapin <1559761+Asapin@users.noreply.github.com>
2025-01-05 14:14:24 +0900

    Added main type aliases

--- src/rust_ffi/src/core/mod.rs
+++ src/rust_ffi/src/core/mod.rs
@@ -4,6 +4,12 @@ pub mod engine_version;
 pub mod layers;
 pub mod vma;
 
+pub type EntityId = u64;
+pub type LayerIndex = u8;
+pub type Priority = u32;
+pub type Hash = u32;
+pub type EntityContainer = Vec<EntityId>;
+
 pub static DEBUG: bool = true;
 pub static SAFE_MODE: bool = false;
 pub static VALIDATE_COLLISION_DATA: bool = DEBUG || SAFE_MODE;
